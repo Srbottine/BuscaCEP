51395d2e8145d5fdf56c70bb9616bd1adeade1cd
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-06-20 15:04:31 +0000

    create css and inputs

--- buscacep/src/App.js
+++ buscacep/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import './css/styles.css'; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  return(
+  <div className="container">
+    <h1 className="title">reset</h1>
+
+    <div className="containerInput">
+      <input 
+      type="text" 
+      placeholder="digite seu CEP..."
+      /> 
+      <button className="buttonSearch">
+        <FiSearch size={25} color='#FFF' />
+      </button>
     </div>
+
+    <main className= "main">
+      <h2>CEP</h2>
+    </main>
+
+  </div>
   );
+  
 }
 
 export default App;
